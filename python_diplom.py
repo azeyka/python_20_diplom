aac2856fa299c9551f6f00abdfd46b4d89f031ec
Elena <42053726+azeyka@users.noreply.github.com>
2018-12-25 20:28:56 +0800

    Update python_diplom.py

--- python_diplom.py
+++ python_diplom.py
@@ -20,7 +20,7 @@ class User():
 
     response = do_api_call('friends.get', params, self.token)
     friends_list = []
-    for friend in response.json()['response']['items']:
+    for friend in response['response']['items']:
       if 'deactivated' not in friend:
         friends_list.append(friend)
 
@@ -32,11 +32,11 @@ class User():
     }
 
     response = do_api_call('users.getSubscriptions', params, self.token)
-    if type(response) is str:
+    if isinstance(response, str):
       print(response)
       return []
     else:
-      return response.json()['response']['groups']['items']
+      return response['response']['groups']['items']
 
   def compare_groups(self):
     print('> Получаем список друзей пользователя...')
@@ -68,7 +68,7 @@ class User():
     bar = '=' * block + ' ' * (bar_length - block)
     text = '\r[{}] ({}%) Выполняется запрос {} из {} (осталось ~{} мин {} сек)'\
             .format(bar, int(progress * 100),  i, friends_count,\
-            int(time_left//60), int(time_left % 60))
+            int(time_left / 60), int(time_left % 60))
     sys.stdout.write(text)
     sys.stdout.flush()
 
@@ -78,18 +78,18 @@ def do_api_call(method, params, token):
   params['access_token'] = token
 
   while True:
-    response = requests.get(method_link, params)
-    if 'error' in response.json():
-      error_code = response.json()['error']['error_code']
-      if error_code == 6 or error_code == 10:
+    response = requests.get(method_link, params).json()
+    if 'error' in response:
+      error_code = response['error']['error_code']
+      if error_code in (TOO_MANY_REQUESTS, FLOOD_CONTROL):
         time.sleep(0.33)
         continue
-      elif error_code == 113:
+      elif error_code == USER_NOT_FOUND:
         return '> Данного пользователя не существует!'
-      elif error_code == 5:
+      elif error_code == BAD_TOKEN:
         return '> Указан не подходящий token в конфигурации!'
-      elif error_code == 30:
-        return '\r\t- Один из друзей пользователя запретил доступ к группам!'
+      elif error_code == PROFILE_IS_PRIVATE:
+        return '\r\t- Один из друзей пользователя запретил доступ к группам!\n'
       else:
         return '> Произошла неизвестная ошибка.'
     else:
@@ -110,10 +110,10 @@ def get_unique_groups():
         token = config['token']
         user_id = config['id']
         response = find_user_in_vk(user_id, token)
-        if type(response) is str:
+        if isinstance(response, str):
           print(response)
         else:
-          parsed_response = response.json()['response'][0]
+          parsed_response = response['response'][0]
           user_name = parsed_response['first_name'] + ' ' + parsed_response['last_name']
           print('> Найден пользователь по имени {}!'.format(user_name))
 
@@ -125,13 +125,14 @@ def get_unique_groups():
             group_ids_str = ','.join([str(id) for id in unique_groups])
             params = {
                 'group_ids': group_ids_str,
+                'fields' : 'members_count'
             }
             response = do_api_call('groups.getById', params, token)
-            if type(response) is str:
+            if isinstance(response, str):
               print(response)
             else:
-              print_unique_groups(response.json()['response'])
-              write_result_to_JSON_file(response.json()['response'])
+              print_unique_groups(response['response'])
+              write_result_to_JSON_file(response['response'])
           else:
             print('\n\nНет ни одной группы в которой бы состоял только {}!'.format(user.name))
 
@@ -163,9 +164,23 @@ def write_result_to_JSON_file(groups):
   print('\n> Записываем результат в файл...')
   file_name_JSON = 'groups.json'
   file_path = os.path.join(current_dir, file_name_JSON)
-  json.dump(groups, open(file_path, 'w', encoding='UTF-8'), sort_keys=True, indent=4, ensure_ascii=False)
+  result = []
+  for group in groups:
+    result.append({
+      'name': group['name'],
+      'gid': group['id'],
+      'members_count': group['members_count']
+    })
+  json.dump(result, open(file_path, 'w', encoding='UTF-8'), indent=4, ensure_ascii=False)
   print('Результат записан в файл по адресу {}'.format(file_path))
 
+TOO_MANY_REQUESTS = 6
+FLOOD_CONTROL = 10
+USER_NOT_FOUND = 113
+BAD_TOKEN = 5
+PROFILE_IS_PRIVATE = 30
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
 if __name__ == '__main__':
-  current_dir = os.path.dirname(os.path.abspath(__file__))
   get_unique_groups()
